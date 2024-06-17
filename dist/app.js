29e634e356a6eae0de2255250fd038b31d77e4e0
zGonzq <gonzaloandres2508@gmail.com>
2024-06-17 00:32:31 -0400

    cambios

--- dist/app.js
+++ dist/app.js
@@ -11,6 +11,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, '../public')));
+console.log(path.join(__dirname, '../public'));
 app.use(express.urlencoded({ extended: true }));
 app.use('/', indexRouter_1.default);
 app.use('/options', optionsRoutes_1.default);
